fcd605a1005560c84a37a45dd61d6b1bebcd306f
Ana Tartari <anatartari412@gmail.com>
2020-10-13 17:32:57 -0300

    Projeto console para teste de arquivo

---
+++ Testes Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Testes_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           StreamWriter sr = new StreamWriter(@"D:\Projetos\GeradorGrafos\grafo.txt");
+
+            sr.WriteLine("Hello World!");
+
+            sr.Close();
+
+            string text = File.ReadAllText(@"D:\Projetos\GeradorGrafos\grafo.txt");
+
+            Console.WriteLine("{0}", text);
+
+            //Console.ReadKey();
+        }
+    }
+}
