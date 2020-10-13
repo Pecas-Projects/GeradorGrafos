0902159d6b7e8c6f19393118c35c10ad180b0d6e
AmadoMaria <mariantoniamado@hotmail.com>
2020-10-13 17:24:53 -0300

    Criando Classes
    - Arco
    - Vertice
    - Grafo

---
+++ GeradorGrafosCore/Grafo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeradorGrafosCore
+{
+    class Grafo
+    {
+        public List<Vertice> Vertices { get; set; }
+        public List<Arco> Arcos { get; set; }
+        public bool dirigido { get; set; }
+
+        public void AdicionaVertice( Vertice v)
+        {
+            Vertices.Add(v);
+        }
+
+        public Vertice ProcuraVertice(int idVertice)
+        {
+            foreach (Vertice v in this.Vertices)
+            {
+                if(v.id == idVertice)
+                {
+                    return v;
+                }
+            }
+            return null;
+        }
+
+        public void RemoveVertice( int idVertice)
+        {
+            Vertice v = new Vertice();
+            v = ProcuraVertice(idVertice);
+            this.Vertices.Remove(v);
+        }
+
+        public int CalculaNumVertices()
+        {
+            return this.Vertices.Count;
+        }
+
+        public void AdicionarArco( Arco a)
+        {
+            this.Arcos.Add(a);
+        }
+
+        public Arco ProcuraArco(int idArco)
+        {
+            foreach (Arco a in this.Arcos)
+            {
+                if (a.id == idArco)
+                {
+                    return a;
+                }
+            }
+            return null;
+        }
+
+        public void RemoveArco(int idArco)
+        {
+            Arco a = new Arco();
+            a = ProcuraArco(idArco);
+            this.Arcos.Remove(a);
+        }
+
+        public int CalculaNumArcos()
+        {
+            return this.Arcos.Count;
+        }
+
+    }
+}
---
+++ GeradorGrafosCore/Vertice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeradorGrafosCore
+{
+    class Vertice
+    {
+        public int id { get; set; }
+        public string etiqueta { get; set; }
+    }
+
+
+}
