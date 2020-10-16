1337013fcb7c474cdaf4d7a8f967a5c6c93b9819
Ana Tartari <anatartari412@gmail.com>
2020-10-15 21:21:56 -0300

    Leitura ed arquivo

--- GeradorGrafosUWP/MainPage.xaml.cs
+++ GeradorGrafosUWP/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using GeradorGrafosCore;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
@@ -9,6 +10,7 @@ using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -75,7 +77,11 @@ namespace GeradorGrafosUWP
 
         private void Button_addVertice(object sender, RoutedEventArgs e)
         {
-            if (inputInformacao.Text != "")
+            if (inputInformacao.Text == null)
+            {
+                // Aviso de erro
+            }
+            else
             {
                 Vertice v = new Vertice();
                 v.id = Grafo.CalculaNumVertices() + 1;
@@ -92,12 +98,16 @@ namespace GeradorGrafosUWP
 
         private void Button_AddArco(object sender, RoutedEventArgs e)
         {
-            if (ComboBox_Vertices_Saida.SelectedValue != null && ComboBox_Vertices_Entrada.SelectedValue != null)
+            if (InputPeso.Text == null || ComboBox_Vertices_Saida.SelectedValue == null || ComboBox_Vertices_Entrada.SelectedValue == null)
+            {
+                Debug.WriteLine("UIUIUUI");
+                // Aviso de erro
+            }
+            else
             {
                 Arco a = new Arco();
                 a.id = Grafo.CalculaNumArcos() + 1;
-                if (InputPeso.Text != "")
-                    a.peso = int.Parse(InputPeso.Text);
+                a.peso = int.Parse(InputPeso.Text);
                 a.entrada = ComboBox_Vertices_Entrada.SelectedValue as Vertice;
                 a.saida = ComboBox_Vertices_Saida.SelectedValue as Vertice;
 
@@ -106,6 +116,122 @@ namespace GeradorGrafosUWP
                 _arcos.Add(a);
 
                 InputPeso.Text = "";
+            }
+        }
+
+        private async void Button_CarregarGrafo(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                StorageFile arqTxt = await ApplicationData.Current.LocalFolder.GetFileAsync("Grafo.txt");
+
+                string arquivoGrafo = "";
+
+                Grafo grafo = new Grafo();
+
+                List<Vertice> verticesList = new List<Vertice>();
+
+                List<Arco> arcosList = new List<Arco>();
+
+
+                using (StreamReader leitura = new StreamReader(await arqTxt.OpenStreamForReadAsync()))
+                {
+                    arquivoGrafo = leitura.ReadToEnd();
+                }
+
+                string[] vetorArqGrafo = arquivoGrafo.Split("*");
+
+                string[] vertices = vetorArqGrafo[1].Split("\r\n");
+
+                string[] arcos = vetorArqGrafo[2].Split("\r\n");
+
+                if (arcos[0].Equals("Edges"))
+                {
+                    grafo.dirigido = false;
+                }
+                else if (arcos[0].Equals("Arcs"))
+                {
+                    grafo.dirigido = true;
+                }
+
+                BuscarVertices(verticesList, vertices);
+
+                BuscarArcos(arcosList, verticesList, arcos);
+
+                grafo.Arcos = arcosList;
+                grafo.Vertices = verticesList;
+
+                this.Grafo = grafo;
+
+                this.Frame.Navigate(typeof(InformacoesGrafo), Grafo);
+
+            }
+            catch(Exception ex)
+            {
+                //Algo no front falando que nao salvou o grafo;
+                Debug.WriteLine("Nenhum grafo salvo");
+            }
+        }
+
+        private void BuscarArcos(List<Arco> arcosList, List<Vertice> verticesList, string[] arcos)
+        {
+            int ID = 0;
+            for (int v = 1; v < arcos.Length; v++)
+            {
+                if (!arcos[v].Equals(""))
+                {
+                    string[] arc = arcos[v].Split(" ");
+
+                    Arco arcoAux = new Arco();
+
+                    int SaidaId = int.Parse(arc[0]);
+
+                    int EntradaId = int.Parse(arc[1]);
+
+                    int Peso = int.Parse(arc[2]);
+
+                    ID++;
+
+                    foreach (Vertice ver in verticesList)
+                    {
+
+                        if (ver.id == SaidaId)
+                        {
+                            arcoAux.saida = ver;
+
+                        }
+
+                        if (ver.id == EntradaId)
+                        {
+                            arcoAux.entrada = ver;
+                        }
+                    }
+
+                    arcoAux.id = ID;
+                    arcoAux.peso = Peso;
+                    arcosList.Add(arcoAux);
+                }
+            }
+        }
+
+        private void BuscarVertices(List<Vertice> verticesList, string[] vertices)
+        {
+            for (int v = 0; v < vertices.Length; v++)
+            {
+                if (v > 0 && !vertices[v].Equals(""))
+                {
+                    string[] ver = vertices[v].Split(" ");
+
+                    int idVertice = int.Parse(ver[0]);
+
+                    string etiquetaVertice = ver[1];
+
+                    verticesList.Add(new Vertice
+                    {
+                        id = idVertice,
+                        etiqueta = etiquetaVertice
+                    });
+                }
             }
         }
     }
