70e2a26ed6f89aefba8a5b177cbe409cb26e0807
Ana Tartari <anatartari412@gmail.com>
2020-10-15 21:28:44 -0300

    Vertice code

--- GeradorGrafosUWP/InformacoesGrafo.xaml.cs
+++ GeradorGrafosUWP/InformacoesGrafo.xaml.cs
@@ -156,6 +156,11 @@ namespace GeradorGrafosUWP
             }
         }
 
+        private void ButtonCalularComponentes(object sender, RoutedEventArgs e)
+        {
+            //nComponentes.Text = Grafo.DFS().ToString();
+        }
+
         private void NumVertices_SelectionChanged(object sender, RoutedEventArgs e)
         {
 
