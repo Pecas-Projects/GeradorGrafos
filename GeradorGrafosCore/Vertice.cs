cbc25683bbc0cc9f63ddb2466b589357a499ee58
Davi Costa <davi.machado.costa@hotmail.com>
2020-10-16 20:56:45 -0300

    Plotando o grafo
    Falta implementar a plotagem de digrafos

--- GeradorGrafosCore/Vertice.cs
+++ GeradorGrafosCore/Vertice.cs
@@ -8,19 +8,22 @@ namespace GeradorGrafosCore
     {
         public int id { get; set; }
         public string etiqueta { get; set; }
-
         public char Cor { get; set; }
         public int Descoberta { get; set; }
         public int Fechamento { get; set; }
         public Vertice Predecssor { get; set; }
+        public int PosX { get; set; }
+        public int PosY { get; set; }
 
         public List<Vertice> ListaAdjacencia { get; set; }
 
         public Vertice()
         {
+            Random r = new Random();
             this.Predecssor = null;
             this.ListaAdjacencia = new List<Vertice>();
-
+            this.PosX = r.Next(800);
+            this.PosY = r.Next(600);
         }
     }
 
